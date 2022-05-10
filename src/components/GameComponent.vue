e0ba2545fd7599ff9701bd0c4c7fcc934756ed43
Vitor Lamartine <vitor.lamartine@sys10.com.br>
2022-05-10 16:10:34 -0300

    Inital commit

---
+++ src/components/GameComponent.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { defineProps, defineEmits, ref } from 'vue';
+  const props = defineProps({
+      pokemons: null
+  });
+
+  const emit = defineEmits(['startGame']);
+  const guess = ref('');
+  const gameStarted = ref(false);
+
+  const startGame = () => {
+    gameStarted.value = true;
+    emit('startGame');
+  }
+
+  const onInputChange = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    props.pokemons.forEach((pokemon: any) => {
+      if (pokemon.found) {
+        return;
+      }
+
+      if (pokemon.name.toLowerCase() === guess.value.toLowerCase()) {
+        pokemon.found = true;
+        guess.value = "";
+      }
+    })
+  }
+</script>
+
+<template>
+  <div class="game-header">
+    <input
+      :disabled="!gameStarted"
+      class="name-input"
+      v-model="guess"
+      placeholder="Insert the name"
+      @input="onInputChange"
+    />
+    <button class="start-game" v-if="!gameStarted" @click="startGame">
+      Start game
+    </button>
+  </div>
+  <div class="list">
+    <div v-for="pokemon in pokemons" :key="pokemon.number">
+      <div class="pokemon">
+        <span class="pokemon-number">{{ pokemon.number }}</span>
+        <span class="pokemon-name">{{
+          pokemon.found ? pokemon.name : ""
+        }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.game-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  margin: 1rem auto;
+}
+.list {
+  column-count: 4;
+  column-fill: balance;
+  column-gap: 0;
+  width: 80%;
+  margin: 0 auto;
+}
+
+.pokemon {
+  background: #fafad2;
+  border: 1px solid #e6d2fa;
+  width: 100%;
+  color: black;
+}
+
+.pokemon-number {
+  background: #8e6fad;
+  width: 30%;
+  display: inline-block;
+  text-align: center;
+  font-weight: bold;
+}
+.pokemon-name {
+  text-transform: capitalize;
+  display: inline-block;
+  padding-left: 15px;
+}
+
+.break {
+  flex-basis: 100%;
+}
+
+.name-input {
+  height: 50px;
+  border-radius: 20px;
+  padding: 1rem;
+  width: 40%;
+}
+
+.start-game {
+  border-radius: 20px;
+  height: 35px;
+  background: #52ff52;
+}
+</style>
---
+++ src/components/GenSelection.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue';
+
+  const generation = ref(0);
+  defineEmits(['selectGen'])
+// export default defineComponent({
+//   name: 'HelloWorld',
+//   props: {
+//     msg: String,
+//   },
+// });
+</script>
+
+<template>
+  <div>
+    <h4 class="m-auto">
+      Choose the pokemon generation pokedex(or national dex)
+    </h4>
+    <div class="m-auto select-form">
+      <select class="select" v-model="generation">
+        <option disabled value="">Choose pokedex</option>
+        <!-- <option value="1">National</option> -->
+        <option value="2">Gen 1 (Kanto)</option>
+        <!-- <option value="3">Gen 2 (Johto)</option>
+        <option value="4">Gen 3 (Hoenn)</option>
+        <option value="5">Gen 4 (Sinnoh)</option>
+        <option value="6">Gen 5 (Unova)</option>
+        <option value="7">Gen 6 (Kalos)</option>
+        <option value="8">Gen 7 (Alola)</option>
+        <option value="9">Gen 8 (Galar)</option> -->
+      </select>
+      <button class="confirm" @click="$emit('selectGen', generation)">
+        Select gen
+      </button>
+    </div>
+  </div>
+</template>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.m-auto {
+  width: fit-content;
+  margin: auto;
+}
+
+.select {
+  padding: 0.5em;
+  border-radius: 5px;
+  margin-right: 1rem;
+}
+
+.select-form {
+  margin-top: 15px;
+}
+.confirm {
+  padding: 0.5rem;
+  border-radius: 10px;
+  border: 0;
+}
+</style>
